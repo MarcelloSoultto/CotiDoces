838f0347b7ed6214669771216a1067f0c998e161
MarcelloSoultto <marcello_soultto@yahoo.com.br>
2025-03-20 23:07:11 -0300

    Delete www.cotidoces directory

--- www.cotidoces/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('pedido-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Seu pedido foi enviado com sucesso!');
-    this.reset(); // Limpa o formulário após o envio
-});
-
-function scrollToSection(sectionId) {
-    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
-}
